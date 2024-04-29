e9fbe2d3daf5dcd4aec33aa0ae722a3a9adb6663
nascimentolds <nascimentolds@hotmail.com>
2024-04-29 16:27:07 -0300

    api

--- src/components/Shorten.jsx
+++ src/components/Shorten.jsx
@@ -1,15 +1,78 @@
+import { useState } from "react";
+
 export default function Shorten() {
+  const [inputValue, setInputValue] = useState("");
+  const [inputError, setInputError] = useState(false);
+  const [shortUrl, setShortUrl] = useState("");
+  const [error, setError] = useState("");
+
+  function handleInputValue(event) {
+    const value = event.target.value;
+    setInputValue(value);
+  }
+
+  function sendingLink() {
+    if (inputValue === "") {
+      setInputError(true);
+    } else {
+      console.log("Gerar url");
+      handleShorten()
+      setInputError(false);
+      setInputValue("");
+    }
+  }
+
+  const handleShorten = async () => {
+    try {
+      const response = await fetch('https://cleanuri.com/api/v1/shorten', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ url: inputValue })
+      })
+
+      if(!response.ok) {
+        throw new Error("Error, try again!")
+      }
+
+      const data = await response.json()
+      setShortUrl(data.result_url)
+
+    } catch (error) {
+      setError(error.message);
+    }
+  }
+
+  console.log(shortUrl)
+
   return (
-    <div className="shorten">
+    <form className="shorten">
       <div className="shorten--form">
-        <input
-          className="shorten--input"
-          type="text"
-          placeholder="Shorten a link here..."
-        />
-        <button className="shorten--buttom">Shorten It!</button>
+        <div className="shorten--input-box">
+          <input
+            className={`shorten--input ${inputError && "shorten--input-erro"}`}
+            type="text"
+            placeholder="Shorten a link here..."
+            value={inputValue}
+            onChange={handleInputValue}
+          />
+          {inputError && (
+            <span className="shorten--erro">
+              <i>Please add a link</i>
+            </span>
+          )}
+        </div>
+        <div>
+          <button
+            onClick={sendingLink}
+            type="button"
+            className="shorten--buttom"
+          >
+            Shorten It!
+          </button>
+        </div>
       </div>
-      <span></span>
-    </div>
+    </form>
   );
 }
